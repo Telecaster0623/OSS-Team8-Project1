e44582d5fa1e12f703095248f5f3ee1bb94e3dc4
joheonkr <chorongi1515@gmail.com>
2025-10-26 01:41:32 +0900

    Update practice.c (fix typo)

--- practice.c
+++ practice.c
@@ -7,7 +7,7 @@ int main() {
 		printf("Enter your student ID: ");
 		while (scanf("%d", &n) == 1) {
 			if (n == 2025046025) printf("Your name is Kim Hunhui\n");
-			else if (n == 2025046007) printf("Your name is Hone seonggwon\n");
+			else if (n == 2025046007) printf("Your name is Hong seonggwon\n");
 			else printf("There are no saved IDs");
 		}
 		break;
