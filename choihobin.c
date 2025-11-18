cdeb08710c6f32e808bf06f7e4ee3c03884f3223
choihobin <151015809+choihobin123@users.noreply.github.com>
2025-11-18 20:28:31 +0900

    no message

--- choihobin.c
+++ choihobin.c
@@ -4,16 +4,37 @@
 
 
 
-int question(char question[10000], int num, char choice_statement[5][10000]){
+int question(const char* questionText, int choiceCount, char choice_statement[5][10000]) {
+    int choice = -1;
+
     system("cls");
     printf("___________________________\n");
-    printf("%s\n",question);
+    printf("%s\n", questionText);
     printf("___________________________\n");
-    for(int i = 0; i < num; i++){
-        printf("%d. %s\n", i + 1 , choice_statement[i]);
+
+    for (int i = 0; i < choiceCount; i++) {
+        printf("%d. %s\n", i + 1, choice_statement[i]);
     }
+
     printf("___________________________\n");
-    printf("--->\n");
-    scanf_s("%d",&num);
-    return num;
+
+    while (1) {
+        printf("---> ");
+        if (scanf_s("%d", &choice) != 1) {
+            // 숫자가 아닐 때 처리
+            printf("숫자를 입력하세요.\n");
+            // 버퍼 비우기
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+            continue;
+        }
+
+        if (choice >= 1 && choice <= choiceCount) {
+            break;
+        } else {
+            printf("1~%d 사이의 번호를 입력하세요.\n", choiceCount);
+        }
+    }
+
+    return choice;
 }
