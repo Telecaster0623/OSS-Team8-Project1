71b86256d8f9bf62b51bab98c850ebbf4b20d644
Telecaster0623 <gnsgml060623@gmail.com>
2025-11-18 19:44:01 +0900

    startScreen

---
+++ startScreen.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+void startScreen() {
+    printf("==============================\n");
+    printf("       시작하시겠습니까?(Y/N)       \n");
+    printf("==============================\n\n");
+}
