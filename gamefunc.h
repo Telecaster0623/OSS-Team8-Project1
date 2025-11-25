f68824b285503ef5395a631f2c3b7bd38b0ae6b3
Telecaster0623 <gnsgml060623@gmail.com>
2025-11-25 22:03:18 +0900

    선택지함수 뼈대만

---
+++ gamefunc.h
@@ -0,0 +1,53 @@
+#ifndef GAMEFUNC_H
+#define GAMEFUNC_H
+
+//1학년
+void y1_q01();
+void y1_q02();
+void y1_q03();
+void y1_q04();
+void y1_q05();
+void y1_q06();
+void y1_q07();
+void y1_q08();
+void y1_q09();
+void y1_q10();
+void y1_q11();
+
+//2학년
+void y2_q01();
+void y2_q02();
+void y2_q03();
+void y2_q04();
+void y2_q05();
+void y2_q06();
+void y2_q07();
+void y2_q08();
+void y2_q09();
+void y2_q10();
+
+//3학년
+void y3_q01();
+void y3_q02();
+void y3_q03();
+void y3_q04();
+void y3_q05();
+void y3_q06();
+void y3_q07();
+void y3_q08();
+void y3_q09();
+void y3_q10();
+
+//4학년
+void y4_q01();
+void y4_q02();
+void y4_q03();
+void y4_q04();
+void y4_q05();
+void y4_q06();
+void y4_q07();
+void y4_q08();
+void y4_q09();
+void y4_q10();
+
+#endif
