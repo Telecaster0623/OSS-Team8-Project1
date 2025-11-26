7fa62662e2ea0c663c7c540236f6a18e876a6d31
Telecaster0623 <gnsgml060623@gmail.com>
2025-11-26 15:24:52 +0900

    훈희씨 함수입니당

--- gamefunc.h
+++ gamefunc.h
@@ -1,53 +1,31 @@
 #ifndef GAMEFUNC_H
 #define GAMEFUNC_H
 
-//1학년
-void y1_q01();
-void y1_q02();
-void y1_q03();
-void y1_q04();
-void y1_q05();
-void y1_q06();
-void y1_q07();
-void y1_q08();
-void y1_q09();
-void y1_q10();
-void y1_q11();
+typedef struct {
+    const char *question;
+    const char *choice1;
+    const char *choice2;
+    const char *choice3;
+    int stateChange1[3];
+    int stateChange2[3];
+    int stateChange3[3];
+    int next1;
+    int next2;
+    int next3;
+} Event;
 
-//2학년
-void y2_q01();
-void y2_q02();
-void y2_q03();
-void y2_q04();
-void y2_q05();
-void y2_q06();
-void y2_q07();
-void y2_q08();
-void y2_q09();
-void y2_q10();
-
-//3학년
-void y3_q01();
-void y3_q02();
-void y3_q03();
-void y3_q04();
-void y3_q05();
-void y3_q06();
-void y3_q07();
-void y3_q08();
-void y3_q09();
-void y3_q10();
-
-//4학년
-void y4_q01();
-void y4_q02();
-void y4_q03();
-void y4_q04();
-void y4_q05();
-void y4_q06();
-void y4_q07();
-void y4_q08();
-void y4_q09();
-void y4_q10();
+// 이벤트 목록
+static const Event eventList[] = {
+    {
+        "당신은 컴퓨터공학과 입학을 앞두고 있습니다.\n어떻게 하시겠습니까?",
+        "강의실에 간다",
+        "카페에서 커피를 마신다",
+        "집에 간다",
+        {0, 0, 5},
+        {4, -3, 45},
+        {40, 53, 4},
+        1, 2, -1   // 다음 이벤트 번호 예시
+    }
+};
 
 #endif
